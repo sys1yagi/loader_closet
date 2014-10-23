94c57b4b7e531f7c641004aae5750820d3f8dae6
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2014-10-24 00:26:23 +0900

    Add sample but not include code.

---
+++ sample/src/main/java/com/sys1yagi/loadercloset/sample/activities/MainActivity.java
@@ -0,0 +1,50 @@
+package com.sys1yagi.loadercloset.sample.activities;
+
+import com.sys1yagi.loadercloset.sample.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    enum Pages {
+        ONE_SHOT_LOADER_ACTIVITY("OneShotLoader with Activity") {
+            @Override
+            public void open(ActionBarActivity activity) {
+                super.open(activity);
+            }
+        },
+        ONE_SHOT_LOADER_FRAGMENT("OneShotLoader with Fragment") {
+            @Override
+            public void open(ActionBarActivity activity) {
+                super.open(activity);
+            }
+        },;
+
+        private String title;
+
+        Pages(String title) {
+            this.title = title;
+        }
+
+        public void open(ActionBarActivity activity) {
+            //
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, -1);
+        for (Pages pages : Pages.values()) {
+            adapter.add(pages.title);
+        }
+        listView.setAdapter(adapter);
+    }
+}
